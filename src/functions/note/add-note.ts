d96ada986dab446d892e0c151bae2d01434965b7
walid <walid.karray@gmail.com>
2022-08-23 14:54:03 +0200

    adding eslint + prettier

--- src/functions/note/add-note.ts
+++ src/functions/note/add-note.ts
@@ -3,20 +3,20 @@ const { stringify, parse } = JSON;
 import { noteRepository } from '../../repositories';
 
 export const handler: Handler = async (
-    event: APIGatewayEvent,
+  event: APIGatewayEvent,
 ): Promise<any> => {
-    try {
-        const body = event.body ? parse(event.body) : null;
-        const note = await noteRepository.addNote(body);
-        return {
-            body: stringify(note),
-            statusCode: 201,
-        };
-    } catch (error) {
-        console.error('Error', error);
-        return {
-            body: 'Internal server error',
-            statusCode: 500,
-        };
-    }
+  try {
+    const body = event.body ? parse(event.body) : null;
+    const note = await noteRepository.addNote(body);
+    return {
+      body: stringify(note),
+      statusCode: 201,
+    };
+  } catch (error) {
+    console.error('Error', error);
+    return {
+      body: 'Internal server error',
+      statusCode: 500,
+    };
+  }
 };
--- src/functions/note/list-notes.ts
+++ src/functions/note/list-notes.ts
@@ -2,17 +2,17 @@ import { APIGatewayEvent, Handler } from 'aws-lambda';
 import { noteRepository } from '../../repositories';
 const { stringify } = JSON;
 export const handler: Handler = async (event: APIGatewayEvent) => {
-    try {
-        const notes = await noteRepository.listNotes();
-        return {
-            body: stringify(notes),
-            statusCode: 200,
-        };
-    } catch (error) {
-        console.error('Error', error);
-        return {
-            body: 'Internal server error',
-            statusCode: 500,
-        };
-    }
+  try {
+    const notes = await noteRepository.listNotes();
+    return {
+      body: stringify(notes),
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error('Error', error);
+    return {
+      body: 'Internal server error',
+      statusCode: 500,
+    };
+  }
 };
