c141a00a036a6c7367fc7f98782ca1eb6126dbdb
walid <walid.karray@gmail.com>
2022-08-23 14:37:09 +0200

    adding listNotes

---
+++ src/functions/note/list-notes.ts
@@ -0,0 +1,18 @@
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+import { noteRepository } from '../../repositories';
+const { stringify } = JSON;
+export const handler: Handler = async (event: APIGatewayEvent) => {
+    try {
+        const notes = await noteRepository.listNotes();
+        return {
+            body: stringify(notes),
+            statusCode: 200,
+        };
+    } catch (error) {
+        console.error('Error', error);
+        return {
+            body: 'Internal server error',
+            statusCode: 500,
+        };
+    }
+};
--- src/repositories/note.repository.ts
+++ src/repositories/note.repository.ts
@@ -1,31 +1,48 @@
 import {
-  DynamoDBClient,
-  PutItemCommand,
-  PutItemCommandInput,
+    DynamoDBClient,
+    PutItemCommand,
+    PutItemCommandInput,
+    ScanCommand,
+    ScanCommandInput,
 } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
+import {marshall, unmarshall} from '@aws-sdk/util-dynamodb';
 import * as uuid from 'uuid';
-import { config } from '../config';
-import { AddNoteDto } from '../dtos/add-note.dto';
-import { Note } from '../models/note.model';
+import {config} from '../config';
+import {AddNoteDto} from '../dtos/add-note.dto';
+import {Note} from '../models/note.model';
 
-const { tableName } = config;
+const {tableName} = config;
 
 export class NoteRepository {
-  constructor(private readonly ddbClient: DynamoDBClient) {}
+    constructor(private readonly ddbClient: DynamoDBClient) {
+    }
 
-  // create a note
-  async addNote(addNoteDto: AddNoteDto): Promise<Note> {
-    const note: Note = {
-      noteId: uuid.v4(),
-      ...addNoteDto,
-    };
-    const input: PutItemCommandInput = {
-      TableName: tableName,
-      Item: marshall(note),
-    };
-    await this.ddbClient.send(new PutItemCommand(input));
-    return note;
-  }
+    // create a note
+    async addNote(addNoteDto: AddNoteDto): Promise<Note> {
+        const note: Note = {
+            noteId: uuid.v4(),
+            ...addNoteDto,
+        };
+        const input: PutItemCommandInput = {
+            TableName: tableName,
+            Item: marshall(note),
+        };
+        await this.ddbClient.send(new PutItemCommand(input));
+        return note;
+    }
+
+    // list all notes
+    async listNotes(): Promise<Note[]> {
+        const params: ScanCommandInput = {
+            TableName: tableName,
+        };
+        const result = await this.ddbClient.send(new ScanCommand(params));
+        const items = result.Items || [];
+        const notes: Note[] = [];
+        for (const item of items) {
+            notes.push(unmarshall(item) as Note);
+        }
+        return notes;
+    }
 
 }
